e694be3e7114d2b4df514451249855a142d1c510
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-03-10 11:52:30 +0700

    inputData

--- Praktikum4.cpp
+++ Praktikum4.cpp
@@ -2,3 +2,12 @@
 #include <string>
 
 using namespace std;
+
+void inputData(float &berat, float &tinggi)
+{
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
